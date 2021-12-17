0492a6bb19d24bb7abab18a4daa95bdcd21b4c5d
5EQQQ <wangzishan456@gmail.com>
2021-12-17 23:01:15 +0000

    implement softmax and initialize structure

--- task3.py
+++ task3.py
@@ -29,3 +29,45 @@ def relu_backward(prev_grad, cache):
         else:
             output.append(0)
     return np.array([output])
+
+# Define a softmax layer
+def softmax(x):
+    x = x.squeeze().astype(float)
+    return np.array([np.exp(x) / sum(np.exp(x))])
+
+
+# Define a single layer and its back propagation
+# Shapes: x-[1, n] w-[n, m] b-[1, m]
+def single_layer(x, w, b):
+    # This is used to store the parameters for back propagation
+    cache = (x, w, b)
+    return cache, np.dot(x, w) + b
+
+
+# The shape of the previous gradient shall be [1, m]
+def single_layer_backward(prev_grad, cache):
+    (x, w, b) = cache
+    # Here we shall calculate the gradient of weight and bias
+    grad_w = np.dot(x.transpose(), prev_grad)
+    grad_b = np.sum(prev_grad, keepdims=True)
+    current_grad = np.dot(prev_grad, w.transpose())
+    grad = (grad_w, grad_b)
+    return current_grad, grad
+
+
+# We shall initialize the parameters based on the structure
+def initializer(structure):
+    weight = []
+    bias = []
+
+    # The total layer shall be n-1, where n denotes the dimension of the structure
+    for i in range(len(structure) - 1):
+        w = np.random.rand(structure[i], structure[i + 1]) / 10
+        b = np.random.rand(1, structure[i + 1]) / 1000
+        weight.append(w)
+        bias.append(b)
+    return weight, bias, len(structure) - 1
+
+
+def cross_entropy(pred, y):
+    return -sum(y * np.log(pred.squeeze()))
