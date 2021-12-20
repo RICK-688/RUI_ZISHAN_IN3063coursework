6f15d1a5228eb321264cb50b027e257da1e40404
rick_yang <rui.yang@city.ac.uk>
2021-12-20 23:32:44 +0000

    initial neural network

--- task3.py
+++ task3.py
@@ -71,3 +71,69 @@ def initializer(structure):
 
 def cross_entropy(pred, y):
     return -sum(y * np.log(pred.squeeze()))
+
+
+
+# The forward and the backward propagation of the network
+def neural_network(x, y, layers, weight, bias, activation_type="relu", requires_cache=True):
+    # This is used to store the parameters
+    # When training, requires_cache is set to true
+    caches = []
+    temp = np.array(x, copy=True)
+    for i in range(layers - 1):
+        cache, temp = single_layer(temp, weight[i], bias[i])
+        if requires_cache:
+            caches.append(cache)
+
+        # Apply the activation function
+        if activation_type == "relu":
+            temp = relu(temp)
+        elif activation_type == "sigmoid":
+            temp = sigmoid(temp)
+        else:
+            raise ValueError("No valid activation type")
+
+    # The last layer is connected with softmax function
+    cache, temp = single_layer(temp, weight[-1], bias[-1])
+    if requires_cache:
+        caches.append(cache)
+    output = softmax(temp)
+    if requires_cache:
+        caches.append((output, y))
+
+    # Use output to calculate the loss value
+    loss = cross_entropy(output, y)
+
+    # Return the predictions, the loss value and the caches
+    return output, loss, caches
+
+
+# The backward propagation of the network
+# Caches store the variables in each layer and have the form of (x, w, b), despite of the last layer
+def neural_network_backward(layers, caches, activation_type="relu"):
+    # Use the last layer in the caches to initialize the gradient
+    grads = []
+    cache = caches[-1]
+    (output, y) = cache
+    current_grad = np.array([output.squeeze() - y])
+    current_grad, grad = single_layer_backward(current_grad, caches[-2])
+    grads.append(grad)
+
+    # Deal with the rest of the layers
+    for i in range(layers - 1):
+        cache = caches[-i - 3]
+
+        if activation_type == "relu":
+            current_grad = relu_backward(current_grad, cache)
+        elif activation_type == "sigmoid":
+            current_grad = sigmoid_backward(current_grad, cache)
+        else:
+            raise ValueError("No valid activation type")
+
+        # Backprop in layers
+        current_grad, grad = single_layer_backward(current_grad, cache)
+
+        # Save the gradient
+        grads.append(grad)
+
+    return grads
